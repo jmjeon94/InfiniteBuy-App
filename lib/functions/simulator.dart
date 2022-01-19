14764a78f4b10797d3f135f326a924a63d82e27b
jmjeon94 <jmjeon3155@gmail.com>
2022-01-19 22:19:07 +0900

    [FIX] v2.1 후반전 매수방법 수정

--- lib/functions/simulator.dart
+++ lib/functions/simulator.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 import 'calculate_n.dart';
 
 var data = [
@@ -44,7 +42,7 @@ class Account {
 
   @override
   toString() {
-    return '보유수량: $n, 평단가: $avg_price, 매수금: $buy_balance, 수익: $profit';
+    return '보유수량: $n, 평단가: ${avg_price.toStringAsFixed(2)}, 평단가10%: ${(avg_price * 1.1).toStringAsFixed(2)}, 매수금: ${buy_balance.toStringAsFixed(2)}, 수익: $profit';
   }
 }
 
@@ -58,14 +56,13 @@ Map simulate(
   // List low_price = data['low'];
   List high_price = data['high'];
   List avg_price = [];
-  var n_buy, n_sell;
-  Account acc = Account();
 
+  Account acc = Account();
   for (int i = 0; i < date.length; i++) {
     // =============매수조건============
     // [Version 1] 0.5회 평단 loc매수, 0.5회 큰수(시장+10%) loc매수
     if (version == '1') {
-      n_buy = calc_n_buy(
+      var n_buy = calc_n_buy(
           ratio: [0.5, 0.5],
           invest_balance: invest_balance,
           cur_price: close_price[i]);
@@ -76,21 +73,34 @@ Map simulate(
       acc.buy(price: close_price[i], n_buy: n_buy[1]);
 
       if (debugPrint) {
-        print('[Buy ver1] $acc');
+        print(
+            '[Buy ver1] ${date[i]} 종가:${close_price[i].toStringAsFixed(2)} $acc');
       }
     }
-    // [Version 1] 0.5회 평단 loc매수, 0.5회 평단+5% loc매수
+    // [Version 2.1] 0.5회 평단 loc매수, 0.5회 평단+5% loc매수
+    // 전반: 평단0%, 평단5%, 후반: 평단0%
     else if (version == '2.1') {
-      n_buy = calc_n_buy(
-          ratio: [0.5, 0.5],
-          invest_balance: invest_balance,
-          cur_price: close_price[i]);
+      bool isBeforeHalf = acc.buy_balance < invest_balance ~/ 2;
+      if (isBeforeHalf) {
+        var n_buy = calc_n_buy(
+            ratio: [0.5, 0.5],
+            invest_balance: invest_balance,
+            cur_price: close_price[i]);
+        if (i == 0 || close_price[i] <= acc.avg_price) {
+          acc.buy(price: close_price[i], n_buy: n_buy[0]);
+        }
+        if (i == 0 || close_price[i] <= acc.avg_price * 1.05) {
+          acc.buy(price: close_price[i], n_buy: n_buy[1]);
+        }
+      } else {
+        var n_buy = calc_n_buy(
+            ratio: [1.0],
+            invest_balance: invest_balance,
+            cur_price: close_price[i]);
 
-      if (i == 0 || close_price[i] <= acc.avg_price) {
-        acc.buy(price: close_price[i], n_buy: n_buy[0]);
-      }
-      if (i == 0 || close_price[i] <= acc.avg_price * 1.05) {
-        acc.buy(price: close_price[i], n_buy: n_buy[1]);
+        if (close_price[i] <= acc.avg_price) {
+          acc.buy(price: close_price[i], n_buy: n_buy[0]);
+        }
       }
 
       if (debugPrint) {
@@ -100,19 +110,19 @@ Map simulate(
 
     // =============매도조건============
     // [Version 1] 평단10% 지정가 전체매도
-    if (i!=0 && version == '1') {
+    if (i != 0 && version == '1') {
       // 평단 10% 지정가 전체 매도
       if (high_price[i] >= acc.avg_price * 1.1) {
         acc.sell(price: acc.avg_price * 1.1, n_sell: acc.n);
 
         if (debugPrint) {
-          print('[Sell ver1] $acc');
+          print('[Sell ver1] ${date[i]} $acc');
         }
       }
     }
     // [Version 2.1] 전반전: 평단5% loc매도(25%), 평단10% 지정가매도(75%)
     //               후반전: 평단0% loc매도(25%), 평단5% 지정가매도(25%), 평단10% 지정가매도(50%)
-    else if (i!=0 && version == '2.1') {
+    else if (i != 0 && version == '2.1') {
       bool isBeforeHalf = acc.buy_balance < invest_balance ~/ 2;
       if (isBeforeHalf) {
         var n_sell = calc_n_sell(ratio: [0.25, 0.75], n_sell: acc.n);
