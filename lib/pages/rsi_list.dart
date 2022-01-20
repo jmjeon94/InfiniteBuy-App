7ca8ba93a1471cd57d8b92601bf99b2a540525dd
jmjeon94 <jmjeon3155@gmail.com>
2022-01-20 20:59:04 +0900

    [UPDATE] 상태 설명 dialog 추가

--- lib/pages/rsi_list.dart
+++ lib/pages/rsi_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_buy/pages/add_ticker.dart';
 import 'package:infinite_buy/styles/style.dart';
@@ -43,9 +44,9 @@ class _RSIListState extends State<RSIList> {
                     separatorBuilder: (BuildContext context, int index) =>
                         const Divider(
                       color: Colors.white38,
-                          height: 8,
+                      height: 8,
                     ),
-                    itemCount: c.tickers_price_list.length ,
+                    itemCount: c.tickers_price_list.length,
                     itemBuilder: (BuildContext context, int idx) {
                       // // 5번째 인덱스는 배너광고
                       // if (idx == 5) {
@@ -83,40 +84,119 @@ class Header extends StatelessWidget {
       height: 50,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          SizedBox(
+            width: 0,
+          ),
           Container(
-              width: 55,
+              width: 60,
               child: Text(
                 '종목',
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               )),
           Container(
-              width: 30,
-              child: Text(
-                '상태',
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              width: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    '상태',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      print('상태 버튼 clicked');
+
+                      return await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: btnColorGrey,
+                            title: Center(child: Text('상태 설명')),
+                            content: Container(
+                              height: 150,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      get_rsi_icon(-30),
+                                      const Text('  RSI 증감 < -15'),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      get_rsi_icon(-10),
+                                      const Text('  RSI 증감 < -5'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      get_rsi_icon(-5),
+                                      const Text('  RSI 증감 <  0'),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      get_rsi_icon(10),
+                                      const Text('  RSI 증감 >  0'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, true),
+                                child: const Text('확인'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(Icons.help_outline),
+                    color: fontColorTitleGrey,
+                    iconSize: 18,
+                    splashRadius: 18,
+                    padding: EdgeInsets.only(left: 3),
+                    constraints: BoxConstraints(),
+                  )
+                ],
               )),
           Container(
-              width: 30,
+              width: 50,
               child: Text(
-                '기준 RSI',
+                'RSI\n기준',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
               )),
           Container(
-              width: 30,
+              width: 50,
               child: Text(
-                '현재 RSI',
+                'RSI\n현재',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
               )),
           Container(
               width: 90,
               child: Text(
-                'RSI 증감률',
+                'RSI\n증감률',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
               )),
@@ -154,11 +234,11 @@ class RSIWidget extends StatelessWidget {
             // decoration: BoxDecoration(border: Border.all(color: Colors.yellow)),
           ),
           Container(
-            width: 30,
+            width: 50,
             child: get_rsi_icon(rsi_diff),
           ),
           Container(
-              width: 30,
+              width: 50,
               child: Text(
                 '${c.tickers_price_list[idx]['ref_rsi']}',
                 textAlign: TextAlign.center,
